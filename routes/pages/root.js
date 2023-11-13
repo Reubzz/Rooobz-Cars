9105c0520217244096d7b5540aa9215b10c3ee79
Reuben George <reugeorac@gmail.com>
2023-11-13 21:43:10 +0530

    added home page

--- routes/pages/root.js
+++ routes/pages/root.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.send("Hello World")
+    // res.send("Hello World")
+
+    res.render('home/home', {
+        data: "test-data"
+    })
 })
 
 
