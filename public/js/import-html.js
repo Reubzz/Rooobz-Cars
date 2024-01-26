752fff3c369123c936e8e50d75b64f6322963dae
Reuben George <reugeorac@gmail.com>
2024-01-27 00:13:42 +0530

    v0.0.5 #1 - refactored code

--- public/js/import-html.js
+++ public/js/import-html.js
@@ -1,7 +1,7 @@
 $(function () {
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-      var file = 'api/components/' + $(this).data('include')
-      $(this).load(file)
-    })
-})
+	var includes = $("[data-include]");
+	$.each(includes, function () {
+		var file = "api/components/" + $(this).data("include");
+		$(this).load(file);
+	});
+});
