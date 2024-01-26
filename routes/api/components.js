2aa98314201054a87a17418d4866b6f4d8863ec6
Reuben George <reugeorac@gmail.com>
2024-01-27 00:12:50 +0530

    v0.0.5 #2 (code cleanup - removed mistake debug statements)

--- routes/api/components.js
+++ routes/api/components.js
@@ -5,7 +5,6 @@ const path = require('path');
 
 // ! Components
 router.get('/:filename', (req, res) => {    
-    console.log(req.query)
     res.render(`components/${req.params.filename}`, req.query)
 })
 
