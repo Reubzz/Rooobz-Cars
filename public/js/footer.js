218ef09cdb5ea8d04452aaf7403d5f7870a22eea
Reuben George <reugeorac@gmail.com>
2024-02-27 16:37:29 +0530

    v0.1.5 - part 3

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,47 +1,4 @@
-// const form = document.querySelector('form');
-// const email = document.getElementById('subscribe-email').value;
-// const submitBtn = document.getElementById('subscribe-submit')
-// const confirmMessage = document.getElementById('confirm-message');
-// confirmMessage.classList.add('show');
-
-// $('#subscribe-submit').click(async function () {
-//     console.log('hello')
-//     const email = $('subscribe-email').value
-//     console.log($('subscribe-email').value) // ! debug
-//     try {
-//         const res = await fetch(`/api/subscribe`, {
-//             method: 'POST',
-//             body: JSON.stringify({
-//                 email: email,
-//             }),
-//             headers: { 'Content-Type': 'application/json' },
-//         })
-//         const data = await res.json();
-
-//         // ! If unsuccessful
-//         if (res.status == 400) {
-//             showMessage(`Error ${data.error.code} - ${data.error.message}`);
-//             email = '';
-//             return;
-//         }
-
-//         // ! If Success 
-//         if (res.status == 200) {
-//             showMessage(data.status.message);
-//             email = '';
-//             return;
-//         }
-//     } catch (error) {
-//         // ! Unkown Error
-//         showMessage(`Error - ${error.message}`);
-//         email = '';
-//         console.log(error.message)
-//         return;
-//     }
-// })
-
 async function emailSubmit(email) {
-    // const email = $('subscribe-email').value
     try {
         const res = await fetch(`/api/subscribe`, {
             method: 'POST',
