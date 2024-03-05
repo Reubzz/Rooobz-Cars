79d1cac089413563f485c311373700cdcb8f3903
Reuben George <reugeorac@gmail.com>
2024-03-05 17:01:57 +0530

    Minor Typo Fix

--- routes/api/account.js
+++ routes/api/account.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { authCheck } = require('../../middleware/authentication/authentication');
 const { editAccount } = require('../../models/functions/account/editAccount.js')
 
-// ! Components
+// ! Edit Account
 router.post('/edit', authCheck, editAccount)
 
 module.exports = router;
