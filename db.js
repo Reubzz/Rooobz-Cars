ff2ef45b235e19e56461c7d67d9a1e95239c6572
Reuben George <reugeorac@gmail.com>
2024-03-23 13:39:17 +0530

    removed unnecessay code generating warnings

--- db.js
+++ db.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const DB_URI = process.env.DB_URI;
 
-mongoose.MAIN_DB = mongoose.createConnection(DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.MAIN_DB = mongoose.createConnection(DB_URI)
 
 
 module.exports = mongoose;
